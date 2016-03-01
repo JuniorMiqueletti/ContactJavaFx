fd4b3cf0736c6d3cdad4af92c4ca0eb774342f46
Junior Miqueletti <juniormiqueletti@gmail.com>
2016-03-01 18:23:56 -0300

    Ajuste na classe Inicializadora

--- src/br/com/fcv/javafx/juniormiqueletti/ContatoMain.java
+++ src/br/com/fcv/javafx/juniormiqueletti/ContatoMain.java
@@ -14,12 +14,13 @@ public class ContatoMain extends Application{
      @Override
         public void start(Stage primaryStage) throws Exception{
             Parent root = FXMLLoader.load(getClass().getResource("/br/com/fcv/javafx/juniormiqueletti/view/ContatoView.fxml"));
-            primaryStage.setTitle("ContatoFx");
+
             primaryStage.setScene(new Scene(root));
+            primaryStage.setTitle("ContatoFx");
+
             primaryStage.show();
 
         }
-
 
         public static void main(String[] args) {
             launch(args);
